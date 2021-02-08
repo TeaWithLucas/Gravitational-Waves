dd0b61cfad08d4582b1ed712ddd7cb5be5a92ca9
Daniel Benedek <benedekDJ@cardiff.ac.uk>
2021-02-08 19:25:29 +0100

    Add basic functions for mini tasks in ITask

--- Assets/Scripts/GW Scripts/Abstraction/Tasks/ITask.cs
+++ Assets/Scripts/GW Scripts/Abstraction/Tasks/ITask.cs
@@ -3,5 +3,15 @@ using System.Collections.Generic;
 
 public interface ITask
 {
-    
+    bool IsTaskCompleted();
+    bool IsTaskInProgress();
+    void CompleteTask();
+
+
+    string GetTaskTitle();
+    string GetTaskDescription();
+
+    int GetRewardTaskScore();
+
+
 }
