18532a86afcb161ffff28b8bdba170a7b19c9842
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-02-15 19:40:27 +0100

    Add mouse click event trigger.

--- Assets/GWAssets/Scripts/Interaction/Tasks/TaskInteractionController.cs
+++ Assets/GWAssets/Scripts/Interaction/Tasks/TaskInteractionController.cs
@@ -3,40 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(InteractiveGameObject))]
-[RequireComponent(typeof(SphereCollider))]
 public class TaskInteractionController : MonoBehaviour
 {
     public GameObject PlayerObject;
-
-    [Tooltip("Toggle whether this Game Object can be interacted with.")]
-    public bool IsInteractible = true;
-
-    private SphereCollider sphereCollider;
     private InteractiveGameObject interactiveComponent;
 
     // Start is called before the first frame update
     void Start()
     {
         interactiveComponent = GetComponent<InteractiveGameObject>();
-        sphereCollider = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
-    { 
-    }
-
-    /// <summary>
-    /// When another object with a collider enters the trigger.
-    /// </summary>
-    /// <param name="other">The other object's collider compontent.</param>
-    private void OnTriggerEnter(Collider other)
     {
-        if (!IsInteractible) return;
-
-        if (other.gameObject.CompareTag("Player"))
-        {
-            interactiveComponent.Interact();
-        }
+        
     }
 }
