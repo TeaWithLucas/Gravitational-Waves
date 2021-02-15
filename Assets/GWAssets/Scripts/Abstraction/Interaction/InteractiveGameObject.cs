d365475733bf286504d2729efbc8e083ff1ba1d9
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-02-15 18:26:28 +0100

    Add Dummy interaction on player trigger enter.

--- Assets/GWAssets/Scripts/Abstraction/Interaction/InteractiveGameObject.cs
+++ Assets/GWAssets/Scripts/Abstraction/Interaction/InteractiveGameObject.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class InteractiveGameObject : MonoBehaviour, IInteractive
+public class InteractiveGameObject : MonoBehaviour, IInteractible
 {
     public UnityEvent OnInteraction;
 
-    public void OnInteract()
+    public void Interact()
     {
         if (OnInteraction.GetPersistentEventCount() > 0)
         {
--- Assets/GWAssets/Scripts/Interaction/Tasks/TaskInteractionController.cs
+++ Assets/GWAssets/Scripts/Interaction/Tasks/TaskInteractionController.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(InteractiveGameObject))]
+[RequireComponent(typeof(SphereCollider))]
 public class TaskInteractionController : MonoBehaviour
 {
-    private InteractiveGameObject interactiveGameObject;
+    public GameObject PlayerObject;
+
+    [Tooltip("Toggle whether this Game Object can be interacted with.")]
+    public bool IsInteractible = true;
+
+    private SphereCollider sphereCollider;
+    private InteractiveGameObject interactiveComponent;
 
     // Start is called before the first frame update
     void Start()
     {
-        interactiveGameObject = GetComponent<InteractiveGameObject>();
+        interactiveComponent = GetComponent<InteractiveGameObject>();
+        sphereCollider = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
+    { 
+    }
+
+    /// <summary>
+    /// When another object with a collider enters the trigger.
+    /// </summary>
+    /// <param name="other">The other object's collider compontent.</param>
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!IsInteractible) return;
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            interactiveComponent.Interact();
+        }
     }
 }
