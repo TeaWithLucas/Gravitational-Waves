8ede9cc135ffd3b82aeabd0584f04b2d6d8afb2f
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-02-16 21:45:15 +0100

    Add Task and Standard task. Start Player task manager and editor.

---
+++ Assets/Scripts/Editor/PlayerTaskManagerEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerTaskManager))]
+public class PlayerTaskManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+
+}
