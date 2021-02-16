c89a3bf0bd25817ab9f43eeee77c60ab6c4ad4f9
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-02-16 12:13:06 +0100

    Fix Global click interaction of task object. Add task object initial configuration.

--- Assets/GWAssets/Scripts/Interaction/Tasks/TaskInteractionController.cs
+++ Assets/GWAssets/Scripts/Interaction/Tasks/TaskInteractionController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(InteractiveGameObject))]
+[ExecuteInEditMode]
 public class TaskInteractionController : MonoBehaviour
 {
-    public GameObject PlayerObject;
     private InteractiveGameObject interactiveComponent;
+    private bool firstSetup = true;
 
-    // Start is called before the first frame update
     void Start()
     {
-        interactiveComponent = GetComponent<InteractiveGameObject>();
+        if (firstSetup)
+        {
+            interactiveComponent = GetComponent<InteractiveGameObject>();
+            interactiveComponent.triggerRadius = 3f;
+
+            if (!interactiveComponent.triggerKeys.Contains(KeyCode.E))
+                interactiveComponent.triggerKeys.Add(KeyCode.E);
+
+            interactiveComponent.IsInteractionAllowed = true;
+            interactiveComponent.CanTriggerByKeys = true;
+            interactiveComponent.CanTriggerByMouse = true;
+        }
+        firstSetup = false;
     }
 
     // Update is called once per frame
