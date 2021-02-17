0ea5098cfcea4fbdc1a8da78bcdd47c416dbba02
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-02-17 16:27:22 +0100

    Add LeanTween package. Add Task list header and animations.

---
+++ Assets/Scripts/UI/UIUtils/TaskListArrow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskListArrow : UIUtils
+{
+    public float RotateOnZBy = 90f;
+    public float RotationDuration = 0.2f;
+
+    private float _nextRotation;
+
+    private new void Awake()
+    {
+        base.Awake();
+
+        _nextRotation = RotateOnZBy;
+    }
+
+    /// <summary>
+    /// Toggles the rotation of the Task List Arrow.
+    /// </summary>
+    public void ToggleRotation()
+    {
+        if (!gameObject.LeanIsTweening())
+        {
+            RotateZBy(_nextRotation, RotationDuration);
+            _nextRotation *= -1;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIUtils/UIUtils.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(UIBehaviour))]
+public class UIUtils : MonoBehaviour
+{
+
+    private UIBehaviour uiBehaviour;
+    private bool _visible = false;
+
+    protected void Awake()
+    {
+        uiBehaviour = GetComponent<UIBehaviour>();
+    }
+
+    /// <summary>
+    /// Toggles whether a UI component is visible or not.
+    /// </summary>
+    public void ToggleVisibility()
+    {
+        _visible = !_visible;
+
+        uiBehaviour.enabled = _visible;
+    }
+
+    public void RotateZBy(float degrees, float duration)
+    {
+        LeanTween.rotate(gameObject, new Vector3(0, 0, gameObject.transform.rotation.eulerAngles.z + degrees), duration);
+    }
+}
