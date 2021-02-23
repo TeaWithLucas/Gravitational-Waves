e6819afd2dfe7d54096171fb58cde1a49791b412
Soma Somorjai <somorjais@cardiff.ac.uk>
2021-02-23 12:29:36 +0100

    Add task list populate. Hide task list better.

--- Assets/Scripts/Player/PlayerTaskManager.cs
+++ Assets/Scripts/Player/PlayerTaskManager.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerTaskManager : MonoBehaviour
 {
     public int numberOfTasks;
 
-    public List<ITask> AssignedTasks = new List<ITask>();
+    [Space]
+
+    [Header("UI Components")]
+    public GameObject TaskListContainer;
+    public GameObject TaskListRowPrefab;
+
+    private List<ITask> _assignedTasks = new List<ITask>();
 
     public void Start()
     {
         for (int i = 0; i < numberOfTasks; i++)
         {
-            AssignedTasks.Add(DummyTaskRepository.GetRandomTask());
+            var task = DummyTaskRepository.GetRandomTask();
+            _assignedTasks.Add(task);
+
+            var taskRow = Instantiate(TaskListRowPrefab, TaskListContainer.transform);
+            taskRow.GetComponentInChildren<TMP_Text>().text = task.GetTitle();
         }
 
-        Debug.Log(AssignedTasks[0].GetTitle());
+        Debug.Log(_assignedTasks[0].GetTitle());
     }
+
+    public List<ITask> GetAssignedTasks() => _assignedTasks;
 }
