2fb8dd489131e811b7e1a6c8ac12dd96fa06f2c0
Daniel Benedek <benedekDJ@cardiff.ac.uk>
2021-03-06 18:09:53 +0100

    Set up keycard task script with onEnable and ButtonClick logic

---
+++ Assets/Scripts/Tasks/KeycardTask.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeycardTask : MonoBehaviour
+{
+    protected Text _inputCode;
+
+    protected Text _cardCode;
+
+    protected static int _codeLength = 5;
+
+    protected static float _codeResetTimeInSeconds = 0.5f;
+
+    private bool _isResetting = false;
+
+    private void OnEnable()
+    {
+        string code = string.Empty;
+
+        for (int i = 0; i < _codeLength; i++)
+        {
+            code += Random.Range(1, 10);
+        }
+
+        _cardCode.text = code;
+        _inputCode.text = string.Empty;
+    }
+
+    public void ButtonClick(int number)
+    {
+        if (_isResetting) { return; }
+
+        _inputCode.text += number;
+
+        if (_inputCode.text == _cardCode.text)
+        {
+            _inputCode.text = "Correct";
+            StartCoroutine(ResetCode());
+        }
+        
+    }
+
+}
