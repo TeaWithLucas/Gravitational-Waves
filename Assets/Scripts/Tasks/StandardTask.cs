1f12e19f19c8a045522e9d4bfb56d3df792f40bd
Daniel Benedek <benedekDJ@cardiff.ac.uk>
2021-03-14 19:58:38 +0100

    Add general task handler for json reading

--- Assets/Scripts/Tasks/StandardTask.cs
+++ Assets/Scripts/Tasks/StandardTask.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System;
+using Newtonsoft.Json;
 
 namespace Game.Tasks {
     [Serializable]
     public class StandardTask : Task {
+
+        [JsonConstructor]
+        public StandardTask(){
+        }
         public StandardTask(string id, string title, string description, string prefab, string reward) : base(id, title, description, prefab, reward) {
         }
 
