a0e2d1cdcfbdd972f23c2733e425a1cd0d4e0933
KiyanSomji <somjik@cardiff.ac.uk>
2021-03-16 12:59:31 +0000

    Script fixed

--- Assets/Scripts/Tasks/MirrorCleaning/ChangeColour.cs
+++ Assets/Scripts/Tasks/MirrorCleaning/ChangeColour.cs
@@ -6,29 +6,24 @@ using UnityEngine.UI;
 public class ChangeColour : MonoBehaviour
 
 {
-    public Renderer rend;
+    public Image mirror;
     public Button CleanButton;
     public Button FirstContactButton;
 
     private void Start()
     {
-        FirstContactButton.onClick.AddListener(Cleaning);
-        CleanButton.onClick.AddListener(Clean);
     }
 
     public void Cleaning()
     {
-        rend = GetComponent<Renderer>();
-
-        rend.material.color = new Color(255F, 51F, 153F);
+        Debug.LogFormat("Applied First Contact");
+        mirror.GetComponent<Image>().color = new Color(255F, 51F, 153F);
 
     }
     public void Clean()
     {
-         
-        rend = GetComponent<Renderer>();
-
-        rend.material.color = new Color(255F, 255F, 255F);
+        Debug.LogFormat("Mirror is now cleaned!");
+        mirror.GetComponent<Image>().color = new Color(255F, 255F, 255F);
     }
 
     
