d8d9590a7eb104f3e53f247c841fe9fba0408a5b
KiyanSomji <somjik@cardiff.ac.uk>
2021-03-18 16:00:27 +0000

    Network added to script to make the player get followed by the minimap

--- Assets/Scripts/UI/InGame/Minimap.cs
+++ Assets/Scripts/UI/InGame/Minimap.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+
 
 public class Minimap : MonoBehaviour
 {
-    public Transform player;
+    public NetworkConnectionToClient LocalClientConn { get => NetworkServer.localConnection; }
+    public Transform LocalClientTransform { get => LocalClientConn.identity.transform; }
 
     private void LateUpdate()
     {
-        Vector3 newPosition = player.position;
+        Vector3 newPosition = LocalClientTransform.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
 
-        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0F);
+        transform.rotation = Quaternion.Euler(90f, LocalClientTransform.eulerAngles.y, 0F);
     }
 }
